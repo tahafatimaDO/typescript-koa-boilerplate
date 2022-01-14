53f6fdc1b78a925f835b0ca226e55c8bcf0fd96a
bitzr01 <info@bitzr.nl>
2022-01-14 23:45:03 +0100

    test

--- src/server/KoaServer.ts
+++ src/server/KoaServer.ts
@@ -30,7 +30,7 @@ export default class KoaServer {
   private healthMonitor: HealthMonitor;
 
   constructor(logger: pino.Logger) {
-    this.app = new Koa();
+    this.app = new Koa() as Koa;
     this.logger = logger;
     this.healthMonitor = new HealthMonitor();
   }
